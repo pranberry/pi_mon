2eeca88922a03c85c95599782ba5c68d5044ed68
pranberry <180306401+pranberry@users.noreply.github.com>
2025-09-04 19:23:20 -0400

    update

--- pi_mon.sh
+++ pi_mon.sh
@@ -4,66 +4,73 @@ LOGFILE="/dev/shm/undervolt.html"
 STATUS=$(vcgencmd get_throttled | awk -F= '{print $2}')
 TEMP=$(vcgencmd measure_temp | awk -F= '{print $2}')
 if [[ "$STATUS" != "0x0" ]]; then
-    {
-    echo "<html><head><meta http-equiv='refresh' content='30'>"
-    echo "<title>Undervoltage Monitor</title></head><body>"
-    echo "<table>
-          <tbody>
-            <tr>
-              <td>Last Updated</td>
-              <td>$(date)</td>
-            </tr>
-            <tr>
-              <td>vcgencmd (throttling status)</td>
-              <td>$STATUS</td>
-            </tr>
-            <tr>
-              <td>Temp</td>
-              <td>$TEMP</td>
-            </tr>
-            <tr>
-              <td>iotop (top I/O processes)</td>
-              <td><pre>$(iotop -b -n 1 -o 2>&1)</pre></td>
-            </tr>
-            <tr>
-              <td>uptime</td>
-              <td>$(uptime)</td>
-            </tr>
-            <tr>
-              <td>free -h (memory)</td>
-              <td><pre>$(free -h)</pre></td>
-            </tr>
-          </tbody>
-        </table>"
-    echo "<h3>===========================</h3>"
-    echo "</body></html>"
-    } >> "$LOGFILE"
+cat <<EOF >> "$LOGFILE"
+<html>
+<head><meta http-equiv='refresh' content='30'>
+<title>Undervoltage Monitor</title></head>
+<body>
+<table>
+  <tbody>
+    <tr>
+      <td>Last Updated</td>
+      <td>$(date)</td>
+    </tr>
+    <tr>
+      <td>vcgencmd (throttling status)</td>
+      <td>$STATUS</td>
+    </tr>
+    <tr>
+      <td>Temp</td>
+      <td>$TEMP</td>
+    </tr>
+    <tr>
+      <td>iotop (top I/O processes)</td>
+      <td><pre>$(iotop -b -n 1 -o 2>&1)</pre></td>
+    </tr>
+    <tr>
+      <td>uptime</td>
+      <td>$(uptime)</td>
+    </tr>
+    <tr>
+      <td>free -h (memory)</td>
+      <td><pre>$(free -h)</pre></td>
+    </tr>
+  </tbody>
+</table>
+<h3>===========================</h3>
+</body>
+</html>
+EOF
+
 else
-    {
-        echo "<html><head><meta http-equiv='refresh' content='30'>"
-        echo "<title>Undervoltage Monitor</title></head><body>"
-        echo "<table>
-          <tbody>
-            <tr>
-              <td>Last Updated: </td>
-              <td>$(date)</td>
-            </tr>
-            <tr>
-              <td>vcgencmd (throttling status): </td>
-              <td>$STATUS</td>
-            </tr>
-            <tr>
-              <td>Temp: </td>
-              <td>$TEMP</td>
-            </tr>
-            <tr>
-              <td>free -h (memory): </td>
-              <td><pre>$(free -h)</pre></td>
-            </tr>            
-          </tbody>
-        </table>"
-        echo "<h3>===========================</h3>"
-        echo "</body></html>"
-    } >> "$LOGFILE"
+cat <<EOF >> "$LOGFILE"
+<html>
+<head><meta http-equiv='refresh' content='30'>
+<title>Undervoltage Monitor</title></head>
+<body>
+<table>
+  <tbody>
+    <tr>
+      <td>Last Updated:</td>
+      <td>$(date)</td>
+    </tr>
+    <tr>
+      <td>vcgencmd (throttling status):</td>
+      <td>$STATUS</td>
+    </tr>
+    <tr>
+      <td>Temp:</td>
+      <td>$TEMP</td>
+    </tr>
+    <tr>
+      <td>free -h (memory):</td>
+      <td><pre>$(free -h)</pre></td>
+    </tr>
+  </tbody>
+</table>
+<h3>===========================</h3>
+</body>
+</html>
+EOF
 fi
 
