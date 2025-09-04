e70091111168cd7de2d43e967170ad90389ab368
pranberry <180306401+pranberry@users.noreply.github.com>
2025-09-04 19:01:37 -0400

    update to save log in RAM...oops

--- pi_mon.sh
+++ pi_mon.sh
@@ -1,30 +1,40 @@
 #!/usr/bin/bash
 
-LOGFILE="/opt/pi_monitor/undervolt.html"
+LOGFILE="/dev/shm/undervolt.html"
 STATUS=$(vcgencmd get_throttled | awk -F= '{print $2}')
 TEMP=$(vcgencmd measure_temp | awk -F= '{print $2}')
 if [[ "$STATUS" != "0x0" ]]; then
     {
     echo "<html><head><meta http-equiv='refresh' content='30'>"
     echo "<title>Undervoltage Monitor</title></head><body>"
-    echo "<h1>Undervoltage Monitor</h1>"
-    echo "<p>Last updated: $(date)</p>"
-
-    echo "<p>vcgencmd (throttling status)</p>"
-    echo "<pre>$STATUS</pre>"
-
-    echo "<p>vcgencmd (throttling status)</p>"
-    echo "<pre>$TEMP</pre>"
-
-    echo "<p>iotop (top I/O processes)</p>"
-    echo "<pre>$(iotop -b -n 1 -o 2>&1)</pre>"
-
-    echo "<p>uptime</p>"
-    echo "<pre>$(uptime)</pre>"
-
-    echo "<p>free -h (memory)</p>"
-    echo "<pre>$(free -h)</pre>"
-
+    echo "<table>
+          <tbody>
+            <tr>
+              <td>Last Updated</td>
+              <td>$(date)</td>
+            </tr>
+            <tr>
+              <td>vcgencmd (throttling status)</td>
+              <td>$STATUS</td>
+            </tr>
+            <tr>
+              <td>Temp</td>
+              <td>$TEMP</td>
+            </tr>
+            <tr>
+              <td>iotop (top I/O processes)</td>
+              <td>$(iotop -b -n 1 -o 2>&1)</td>
+            </tr>
+            <tr>
+              <td>uptime</td>
+              <td>$(uptime)</td>
+            </tr>
+            <tr>
+              <td>free -h (memory)</td>
+              <td>$(free -h)</td>
+            </tr>
+          </tbody>
+        </table>"
     echo "<h3>===========================</h3>"
     echo "</body></html>"
     } >> "$LOGFILE"
@@ -33,11 +43,6 @@ else
         echo "<html><head><meta http-equiv='refresh' content='30'>"
         echo "<title>Undervoltage Monitor</title></head><body>"
         echo "<table>
-          <thead>
-            <tr>
-              <th>Undervoltage Monitor</th>
-            </tr>
-          </thead>
           <tbody>
             <tr>
               <td>Last Updated</td>
